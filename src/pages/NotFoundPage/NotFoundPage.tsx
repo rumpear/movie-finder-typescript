d7304e2b192d0c9db0dae0f5af2af1a734562386
rumpear <roman.f.grusha@gmail.com>
2022-09-26 18:05:31 +0300

    NotFoundPage added

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import { GoBackButton } from "../../components/ui/";
+
+export const NotFoundPage = () => {
+  return (
+    <>
+      <GoBackButton />
+      <p>Page not found. {<Link to={"/"}>You can return to the main</Link>}</p>
+    </>
+  );
+};
