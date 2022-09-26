cf7fb2b3f1ce9143a5ca65db1c14ba5a2f2d8797
rumpear <roman.f.grusha@gmail.com>
2022-09-26 18:05:51 +0300

    HomePage added

---
+++ src/pages/HomePage/HomePage.styled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 30px;
+  margin-bottom: 10px;
+`;
+
+export const List = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 20px;
+`;
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,20 @@
+import { useFetchTrendingMovies } from "../../hooks";
+import { ErrorMessage, Gallery } from "../../components";
+import { Loader } from "../../components/ui/";
+import { Title } from "./HomePage.styled";
+
+export const HomePage = () => {
+  const { trendingMovies, isLoading, error } = useFetchTrendingMovies();
+
+  return (
+    <>
+      <Title>Trending movies</Title>
+      {isLoading && !error ? (
+        <Loader />
+      ) : (
+        trendingMovies && <Gallery movies={trendingMovies} />
+      )}
+      {error && <ErrorMessage message={error} />}
+    </>
+  );
+};
