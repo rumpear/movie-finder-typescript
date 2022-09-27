db31c5e946b1584d5ed1308605c6b1c917b8b9de
rumpear <roman.f.grusha@gmail.com>
2022-09-27 12:55:23 +0300

    Navigation added

---
+++ src/components/Navigation/Navigation.styled.tsx
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+export const NavWrapper = styled.nav`
+  background-color: #fff;
+  padding: 10px 0;
+`;
+
+export const Link = styled(NavLink)`
+  color: black;
+  font-size: 30px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+
+  :last-child {
+    margin: 0;
+  }
+
+  &.active {
+    color: red;
+    font-weight: 700;
+  }
+`;
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,10 @@
+import { NavWrapper, Link } from "./Navigation.styled";
+
+export const Navigation = () => {
+  return (
+    <NavWrapper>
+      <Link to="/">Home</Link>
+      <Link to="/movies">Movie</Link>
+    </NavWrapper>
+  );
+};
