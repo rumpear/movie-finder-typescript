cdca40e7460877feb8a19232355ae2c9779c8274
rumpear <roman.f.grusha@gmail.com>
2022-09-28 15:13:53 +0300

    setLazyRoute added

---
+++ src/utils/setLazyRoute.ts
@@ -0,0 +1,8 @@
+import { lazy } from "react";
+
+export const setLazyRoute = (componentName: string) =>
+  lazy(() =>
+    import(`../pages/${componentName}/`).then((module) => ({
+      default: module[componentName],
+    }))
+  );
