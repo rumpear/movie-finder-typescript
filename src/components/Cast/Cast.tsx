c5d327fc8967162381db45c5057679361d7c26f1
rumpear <roman.f.grusha@gmail.com>
2022-09-28 14:26:39 +0300

    Cast upd, types added

--- src/components/Cast/Cast.tsx
+++ src/components/Cast/Cast.tsx
@@ -1,34 +1,18 @@
-import { useParams } from "react-router-dom";
-import { CastList, ErrorMessage } from "..";
-import { Loader } from "../ui";
-import { fetchCast } from "../../services";
-import { useFetch } from "../../hooks";
-
-interface TCast {
-  cast: {
-    adult: boolean;
-    cast_id: number;
-    character: string;
-    credit_id: string;
-    gender: number;
-    id: number;
-    known_for_department: string;
-    name: string;
-    order: number;
-    original_name: string;
-    popularity: number;
-    profile_path: string;
-  }[];
-}
+import { useParams } from 'react-router-dom';
+import { CastList, ErrorMessage } from '..';
+import { Loader } from '../ui';
+import { fetchCast } from '../../services';
+import { useFetch } from '../../hooks/';
+import { ICast } from './types';
 
 export const Cast = () => {
   const { movieId } = useParams();
-  // const [data, loading, error] = useFetch(() => fetchCast(movieId), [movieId]);
-  const castUrl = movieId ? `/movie/${movieId}/credits` : "";
-  const { state, loading, error } = useFetch<TCast>(castUrl);
-  console.log(state, "state===state");
-  // console.log(data?.cast, "Cast");
+  const { state, loading, error } = useFetch<ICast>(
+    () => fetchCast<ICast>(movieId!),
+    [movieId!]
+  );
   const castData = state?.cast;
+
   return (
     <>
       {loading && !error ? (
