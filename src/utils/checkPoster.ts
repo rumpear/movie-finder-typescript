e8ff85306ece7cbb6e0e0e10a84072a8119b4bda
Roman <77742141+rumpear@users.noreply.github.com>
2022-09-29 18:42:49 +0300

    checkPoster added

---
+++ src/utils/checkPoster.ts
@@ -0,0 +1,7 @@
+import { IMAGE_PATH_POSTER, NO_IMAGE } from "../constants";
+
+type TCheckPoster = (posterPath: string) => string;
+
+export const checkPoster: TCheckPoster = (posterPath) => {
+  return posterPath ? IMAGE_PATH_POSTER + posterPath : NO_IMAGE;
+};
