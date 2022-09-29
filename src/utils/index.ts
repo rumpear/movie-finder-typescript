2656164aa978542c0034e891fea512604531f032
Roman <77742141+rumpear@users.noreply.github.com>
2022-09-29 18:43:04 +0300

    checkAvatarPath added

---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+export { getGenres } from "./getGenres";
+export { checkAvatarPath } from "./checkAvatarPath";
+export { checkProfilePhoto } from "./checkProfilePhoto";
+export { checkPoster } from "./checkPoster";
+export { setLazyRoute } from "./setLazyRoute";
