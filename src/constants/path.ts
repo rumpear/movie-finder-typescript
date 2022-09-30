dab84f8dea1438c267ffdb52e66a13a73ccbcaf9
Roman <77742141+rumpear@users.noreply.github.com>
2022-09-30 19:05:11 +0300

    constants added

---
+++ src/constants/path.ts
@@ -0,0 +1,5 @@
+export const NO_IMAGE =
+  'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg';
+export const IMAGE_PATH_ORIGINAL = `https://image.tmdb.org/t/p/original`;
+export const IMAGE_PATH_POSTER = `https://image.tmdb.org/t/p/w500`;
+export const IMAGE_PATH_PROFILE = `https://image.tmdb.org/t/p/w185`;
