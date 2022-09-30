308c9aa413ec3f1924b8a900032d6b2861bf8e5f
Roman <77742141+rumpear@users.noreply.github.com>
2022-09-30 19:02:04 +0300

    MoviesPage refactoring

--- src/pages/MoviesPage/MoviesPage.tsx
+++ src/pages/MoviesPage/MoviesPage.tsx
@@ -1,41 +1,21 @@
-import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
-import { fetchSearchMovie } from "../../services/movieApi";
-import { SearchForm, Gallery, ErrorMessage } from "../../components";
-import { Loader } from "../../components/ui";
+import { useSearchParams } from 'react-router-dom';
+import { SearchForm, Gallery, ErrorMessage } from '../../components';
+import { Loader } from '../../components/ui';
+import { useFetchSearchedMovie } from '../../hooks';
 
 export const MoviesPage = () => {
   const [search, setSearch] = useSearchParams();
-  const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const query = search.get("query");
-
-  useEffect(() => {
-    if (!query) {
-      return;
-    }
-
-    const getData = async () => {
-      setLoading(true);
-
-      try {
-        const { results } = await fetchSearchMovie(query);
-        setMovies(results);
-      } catch (error: any) {
-        setError(error.message);
-      }
-
-      setLoading(false);
-    };
-
-    getData();
-  }, [query, search]);
+  const query = search.get('query');
+  const { movies, loading, error } = useFetchSearchedMovie(query!);
 
   return (
     <>
       <SearchForm onSearch={setSearch} query={query} />
-      {loading && !error ? <Loader /> : movies && <Gallery movies={movies} />}
+      {loading && !error ? (
+        <Loader />
+      ) : (
+        movies && <Gallery movies={movies} />
+      )}
       {error && <ErrorMessage message={error} />}
     </>
   );
